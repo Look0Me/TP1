cadb13c0e83f1ef540597333eca12f77ae31ea9b
Look0Me <100816595+Look0Me@users.noreply.github.com>
2023-11-01 02:01:34 +0300

    Update Queue.cpp

--- TP1/Queue.cpp
+++ TP1/Queue.cpp
@@ -10,18 +10,3 @@ Queue::~Queue()
 	cout << "Base cleared: " << this<< endl;
 }
 
-//void Queue::pop()
-//{
-//	queer.pop_front();
-//}
-
-//void Queue::push()
-//{
-//	queer.push_front();
-//}
-
-//int Queue::getsize()
-//{
-//	size= queer.GetSize();
-//	return size;
-//}
